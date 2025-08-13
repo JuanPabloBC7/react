829b08c455ecf9416ce41bceb9fd0d34eed6d1ce
JuanPabloBC7 <juanpablobc7@gmail.com>
2025-08-13 00:32:10 -0600

    add edit, update and delete functions for table and create a modals.

--- src/components/pages/SimpleCrud.jsx
+++ src/components/pages/SimpleCrud.jsx
@@ -1,14 +1,22 @@
-import Card from 'react-bootstrap/Card';
 import { useState } from 'react';
+import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import AlertNotify from '../common/ui/Alert';
+import Modal from 'react-bootstrap/Modal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SimpleCrud() {
+  const [showViewUserModal, setShowViewUserModal] = useState(false);
+  const [showDeleteUserModal, setShowDeleteUserModal] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleCloseViewUserModal = () => setShowViewUserModal(false);
+  const handleCloseDeleteUserModal = () => setShowDeleteUserModal(false);
+
   // formulario
   const [form, setForm] = useState({ 
     firstName: '', 
@@ -82,6 +90,74 @@ function SimpleCrud() {
       setAlert({ show: false, title: '', message: '' });
     }, 4000);
   };
+
+  const viewUser = (userSelected, idxSelected, modal) => {
+    setUser({
+      firstName: userSelected.firstName,
+      lastName: userSelected.lastName,
+      birthday: userSelected.birthday,
+      email: userSelected.email,
+      description: userSelected.description,
+    });
+
+    if (modal === 'setShowViewUserModal') {
+      setShowViewUserModal(true);
+    } else if (modal === 'setShowDeleteUserModal') {
+      setShowDeleteUserModal(true);
+    }
+  }
+
+  const editUser = (userSelected) => {
+    setIsEditing(true);
+    setForm({
+      firstName: userSelected.firstName,
+      lastName: userSelected.lastName,
+      birthday: userSelected.birthday,
+      email: userSelected.email,
+      description: userSelected.description
+    });
+  }
+
+  const updateUser = () => {
+    if (isFormValid) {
+      const index = users.findIndex(u => u.email === user.email);
+
+      if (index !== -1) {
+        const updatedUsers = [...users];
+        updatedUsers[index] = { ...form };
+        setUsers(updatedUsers);
+        setAlert({ type: 'success', title: 'Success', message: 'User updated successfully!', icon: 'fa-solid fa-circle-check', show: true, });
+      }
+
+      setForm({ firstName: '', lastName: '', birthday: '', email: '', description: '' });
+      setIsEditing(false);
+    } else {
+      setAlert({ type: 'danger', title: 'Error', message: 'Something is wrong, please try again later.', icon: 'fa-solid fa-circle-xmark', show: true, });
+    }
+
+    setTimeout(() => {
+      setAlert({ show: false, title: '', message: '' });
+    }, 4000);
+  }
+
+  const deleteUser = () => {
+    const index = users.findIndex(u => u.email === user.email);
+    if (index !== -1) {
+      users.splice(index, 1);
+
+      setAlert({ type: 'success', title: 'Success', message: 'User deleted successfully!', icon: 'fa-solid fa-circle-check', show: true, });
+    }
+
+    setShowDeleteUserModal(false);
+    setTimeout(() => {
+      setAlert({ show: false, title: '', message: '' });
+    }, 4000);
+  }
+  
+  const clearForm = () => {
+    setForm({ firstName: '', lastName: '', birthday: '', email: '', description: '' });
+    setIsEditing(false);
+  }
 
   return (
     <>
@@ -185,7 +261,14 @@ function SimpleCrud() {
             </Row>
             <Row>
               <Col className='text-center' xs={12}>
-                <Button type="submit" disabled={!isFormValid}>Submit user</Button>
+                { !isEditing ? (
+                  <Button type="submit" disabled={!isFormValid}>Submit user</Button>
+                ) : (
+                  <>
+                    <Button type="button" variant="secondary" className='me-2' onClick={() => clearForm(user)}>Cancel</Button>
+                    <Button type="button" disabled={!isFormValid} onClick={() => updateUser(user)}>Update user</Button>
+                  </>
+                )}
               </Col>
             </Row>
           </Form>
@@ -220,8 +303,8 @@ function SimpleCrud() {
               </tr>
             </thead>
             <tbody>
-              {users.map((user) => (
-                <tr key={user}>
+              {users.map((user, idx) => (
+                <tr key={idx}>
                   <td>{user.firstName}</td>
                   <td>{user.lastName}</td>
                   <td>{user.birthday}</td>
@@ -229,13 +312,13 @@ function SimpleCrud() {
                   <td>
                     <Row className='text-center'>
                       <Col xs={12} sm={4}>
-                        <Button type="button" variant='link'><FontAwesomeIcon icon="fa-solid fa-eye" className='text-primary-emphasis'/></Button>
+                        <Button type="button" variant='link' onClick={() => viewUser(user, idx, 'setShowViewUserModal')}><FontAwesomeIcon icon="fa-solid fa-eye" className='text-primary-emphasis'/></Button>
                       </Col>
                       <Col xs={12} sm={4}>
-                        <Button type="button" variant='link'><FontAwesomeIcon icon="fa-solid fa-pen-to-square" className='text-warning-emphasis'/></Button>
+                        <Button type="button" variant='link' onClick={() => editUser(user)}><FontAwesomeIcon icon="fa-solid fa-pen-to-square" className='text-warning-emphasis'/></Button>
                       </Col>
                       <Col xs={12} sm={4}>
-                        <Button type="button" variant='link'><FontAwesomeIcon icon="fa-solid fa-trash" className='text-danger-emphasis'/></Button>
+                        <Button type="button" variant='link' onClick={() => viewUser(user, idx, 'setShowDeleteUserModal')}><FontAwesomeIcon icon="fa-solid fa-trash" className='text-danger-emphasis'/></Button>
                       </Col>
                     </Row>
                   </td>
@@ -247,6 +330,56 @@ function SimpleCrud() {
             <p className="text-center">There is no data to display in the table.</p> }
         </Card.Body>
       </Card>
+
+      {/* View User Modal */}
+      <Modal show={showViewUserModal} onHide={handleCloseViewUserModal} size="xl">
+        <Modal.Header closeButton>
+          <Modal.Title>User Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row className="mb-3">
+            <Form.Group as={Col} xs={12} sm={6} className='mt-0' controlId="firstname">
+              <Form.Label>First name</Form.Label>
+              <Form.Control type="text" name="firstName" placeholder="First name" value={user.firstName} readOnly/>
+            </Form.Group>
+            <Form.Group as={Col} xs={12} sm={6} className='mt-0' controlId="lastname">
+              <Form.Label>Last name</Form.Label>
+              <Form.Control type="text" name="lastName" placeholder="Last name" value={user.lastName} readOnly/>
+            </Form.Group>
+            <Form.Group as={Col} xs={12} sm={6} className='mt-3' controlId="birthday">
+              <Form.Label>Birthday</Form.Label>
+              <Form.Control type="text" name="birthday" placeholder="Birthday" value={user.birthday} readOnly/>
+            </Form.Group>
+            <Form.Group as={Col} xs={12} sm={6} className='mt-3' controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" name="email" placeholder="Email" value={user.email} readOnly/>
+            </Form.Group>
+            <Form.Group as={Col} xs={12} sm={12} className='mt-3' controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea"  name="description" placeholder="Description" rows={3} value={user.description} readOnly/>
+            </Form.Group>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseViewUserModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Delete User Modal */}
+      <Modal show={showDeleteUserModal} onHide={handleCloseDeleteUserModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Information</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Row className="mb-3">
+            <p as={Col} xs={12} sm={12} className='mb-2'>Are you sure you want to delete the user <strong>{ user.firstName } { user.lastName }</strong>?</p>
+          </Row>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDeleteUserModal}>Close</Button>
+          <Button variant="danger" onClick={deleteUser}>Delete user</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
